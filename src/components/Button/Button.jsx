1d325d06155a24edb88fa535de38527e89beeb90
Seba <gonzaleaugusto@gmail.com>
2023-04-17 06:37:52 -0300

    renovando proyecto myContext

--- src/components/Button/Button.jsx
+++
@@ -1,5 +0,0 @@
-import "./Button.css";
-
-export default function Button(props) {
-  return <button className="button">{props.children}</button>;
-}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
-import "./Header.css";
+import s_ from "@/styles/Header.module.css";
+
+console.log(s_.header);
 
 export default function Header() {
   return (
-    <header className="header">
-      <img src="/img/HeaderHeader_total.png" alt="cabecera" />
+    <header className={s_.header}>
+      <figure className={s_.logo}>
+        <div className={s_.logoIcon}></div>
+      </figure>
+      <nav className={s_.nav}>
+        <ul className={s_.itemsList}>
+          <li className={s_.item}></li>
+          <li className={s_.item}></li>
+          <li className={s_.item}></li>
+        </ul>
+      </nav>
     </header>
   );
 }
